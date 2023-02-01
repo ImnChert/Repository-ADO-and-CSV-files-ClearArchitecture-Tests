1c48f1fbbdd2c5bf9fd02d435051e6c3e514b6e4
Osten Power <osten_power@mail.ru>
2023-02-01 17:05:14 +0300

    create userRepository

--- Data/MSSQLRepository/UserReposiotry.cs
+++ Data/MSSQLRepository/UserReposiotry.cs
@@ -1,6 +1,135 @@
-﻿namespace Data.MSSQLRepository
+﻿using Core.Models;
+using System.Data.SqlClient;
+
+namespace Data.MSSQLRepository
 {
-    public class UserReposiotry
+    public class UserReposiotry : MainRepository<User>
     {
+        private TrophyRepository _trophyRepository;
+        public UserReposiotry(string connectionString, string tableName, string createQuery, string updateQuery,
+            string getQuery, string getAllQuery, TrophyRepository trophyRepository)
+            : base(connectionString, tableName, createQuery, updateQuery, getQuery, getAllQuery)
+        {
+            _trophyRepository = trophyRepository;
+        }
+
+        public override async Task CreateAsync(User entity)
+        {
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+
+                using (var sqlDataAdapter = new SqlDataAdapter(createQuery, sqlConnection))
+                {
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@firstName", entity.FirstName);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@middleName", entity.MiddleName);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@lastName", entity.LastName);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@age", entity.Age);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@trophy", entity.Trophy.Id);
+
+                    sqlDataAdapter.SelectCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public override async Task<List<User>> GetAllAsync()
+        {
+            var users = new List<User>();
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+
+                using (var sqlCommand = new SqlCommand(getAllQuery, sqlConnection))
+                {
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        while (await sqlReader.ReadAsync())
+                        {
+                            int userId = sqlReader.GetInt16(0);
+                            string firstName = sqlReader.GetString(1);
+                            string middleName = sqlReader.GetString(2);
+                            string lastName = sqlReader.GetString(3);
+                            int age = sqlReader.GetInt16(4);
+                            int trophyId = sqlReader.GetInt16(5);
+                            Trophy trophy = await _trophyRepository.GetAsync(trophyId);
+
+                            var user = new User()
+                            {
+                                Id = userId,
+                                FirstName = firstName,
+                                MiddleName = middleName,
+                                LastName = lastName,
+                                Age = age,
+                                Trophy = trophy
+                            };
+
+                            users.Add(user);
+                        }
+                    }
+                }
+            }
+
+            return users;
+        }
+
+        public override async Task<User> GetAsync(int id)
+        {
+            var user = new User();
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+
+                using (var sqlCommand = new SqlCommand(getQuery, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                    {
+                        while (sqlReader.Read())
+                        {
+                            int userId = sqlReader.GetInt16(0);
+                            string firstName = sqlReader.GetString(1);
+                            string middleName = sqlReader.GetString(2);
+                            string lastName = sqlReader.GetString(3);
+                            int age = sqlReader.GetInt16(4);
+                            int trophyId = sqlReader.GetInt16(5);
+                            Trophy trophy = await _trophyRepository.GetAsync(trophyId);
+
+
+                            user.Id = userId;
+                            user.FirstName = firstName;
+                            user.MiddleName = middleName;
+                            user.LastName = lastName;
+                            user.Age = age;
+                            user.Trophy = trophy;
+                        }
+                    }
+                }
+            }
+
+            return user;
+        }
+
+        public override async Task UpdateAsync(User entity)
+        {
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+
+                using (var sqlDataAdapter = new SqlDataAdapter(updateQuery, sqlConnection))
+                {
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@id", entity.Id);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@firstName", entity.FirstName);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@middleName", entity.MiddleName);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@lastName", entity.LastName);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@age", entity.Age);
+                    sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@trophy", entity.Trophy);
+
+                    sqlDataAdapter.SelectCommand.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
