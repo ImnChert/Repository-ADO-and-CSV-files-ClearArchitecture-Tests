7c895d07687ddac6d117d41ca52473cb8a79f846
Osten Power <osten_power@mail.ru>
2023-02-02 09:34:53 +0300

    rebuild

---
+++ HunterWinForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace HunterWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IntegrationTests/MSSQLAnimalServiceRepository.cs
@@ -0,0 +1,16 @@
+using Core.Models;
+
+namespace IntegrationTests
+{
+    public class MSSQLAnimalServiceRepository
+    {
+        private static string _connectionString = @"Data Source=DESKTOP-CTBUCT0\SQLEXPRESS;;Initial Catalog=laba1;Integrated Security=True";
+        private static Animal _testAnimal = new Animal();
+
+        [Fact]
+        public void Anim()
+        {
+
+        }
+    }
+}
